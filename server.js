85731cae0c56eb846821d7d3b837c8d506bc3fe3
Subesh Sapkota <subeshsapkota@gmail.com>
2020-06-06 21:04:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/milkorderonline'));
+app.use(express.static(__dirname + '/dist/mega-front'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/milkorderonline/index.html'));});
+'/dist/mega-front/index.html'));});
 app.listen(process.env.PORT || 8080);
